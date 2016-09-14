2e71cd1684e96abcc2904c5a90aec65c87280bcf
Paul Burz <burzpaul@yahoo.ro>
2016-09-14 10:57:24 +0300

    Numbers in Roman Numerals.

--- CifreRomane/CifreRomane/CifreRomaneTests.cs
+++ CifreRomane/CifreRomane/CifreRomaneTests.cs
@@ -7,35 +7,57 @@ namespace CifreRomane
     public class CifreRomaneTests
     {
         [TestMethod]
-        public void Number()
+        public void SingleDigitConversion()
         {
-            Assert.AreEqual("The number does not exist in Roman Numerals", NumberInRomanNumerals(-5));
+            Assert.AreEqual("IV", NumberInRomanNumerals(4));
         }
-
+        [TestMethod]
+        public void DoubleDigitConversion()
+        {
+            Assert.AreEqual("XXXII", NumberInRomanNumerals(32));
+        }
+        [TestMethod]
+        public void TripleDigitConversion()
+        {
+            Assert.AreEqual("C", NumberInRomanNumerals(100));
+        }
         string NumberInRomanNumerals(int number)
         {
-            if (number <=-1) return "The number does not exist in Roman Numerals";
+            if(IsNumberSingleDigit(number))
+            {
+                if (number >= 9) return "IX" + NumberInRomanNumerals(number - 9);
+                if (number >= 5) return "V" + NumberInRomanNumerals(number - 5);
+                if (number >= 4) return "IV" + NumberInRomanNumerals(number - 4);
+                if (number >= 1) return "I" + NumberInRomanNumerals(number - 1);
+            }
+            if(IsNumberDoubleDigit(number))
+            {
+                if (number >= 90) return "XC" + NumberInRomanNumerals(number - 90);
+                if (number >= 50) return "L" + NumberInRomanNumerals(number - 50);  
+                if (number >= 40) return "XL" + NumberInRomanNumerals(number - 40);
+                if (number >= 10) return "X" + NumberInRomanNumerals(number - 10);
+              
+            }
+            if(IsNumberTripleDigit(number))
+            {
+                if (number >= 100) return "C" + NumberInRomanNumerals(number - 100);
+            }
+            return null;
+        }
 
-            if (number >= 100) return "C" + NumberInRomanNumerals(number - 100);
+        private bool IsNumberTripleDigit(int number)
+        {
+            return number >= 100;
+        }
 
-            if (number >= 90) return "XC" + NumberInRomanNumerals(number - 90);
+        private bool IsNumberDoubleDigit(int number)
+        {
+            return number >= 10 && number <100;
+        }
 
-            if (number >= 50) return "L" + NumberInRomanNumerals(number - 50);
-
-            if (number >= 40) return "XL" + NumberInRomanNumerals(number - 40);
-
-            if (number >= 10) return "X" + NumberInRomanNumerals(number - 10);
-
-            if (number >= 9) return "IX" + NumberInRomanNumerals(number - 9);
-
-            if (number >= 5) return "V" + NumberInRomanNumerals(number - 5);
-
-            if (number >= 4) return "IV" + NumberInRomanNumerals(number - 4);
-
-            if (number >= 1) return "I" + NumberInRomanNumerals(number - 1);
-
-            else
-                return null;
+        private bool IsNumberSingleDigit(int number)
+        {
+            return number < 10;
         }
     }
 }
