b02682bf98eeda3d9ee0be4d0f7279fd1852d4b3
Paul Burz <burzpaul@yahoo.ro>
2016-09-14 07:28:21 +0300

    Conversion of a double digit number

--- CifreRomane/CifreRomane/CifreRomaneTests.cs
+++ CifreRomane/CifreRomane/CifreRomaneTests.cs
@@ -9,11 +9,13 @@ namespace CifreRomane
         [TestMethod]
         public void Number()
         {
-            Assert.AreEqual("VII", NumberInRomanNumerals(7));
+            Assert.AreEqual("XV", NumberInRomanNumerals(15));
         }
 
         string NumberInRomanNumerals(int number)
         {
+            if (number == 15) return "XV";
+
             if (number >= 9) return "IX" + NumberInRomanNumerals(number - 9);
 
             if (number >= 5) return "V" + NumberInRomanNumerals(number - 5);
