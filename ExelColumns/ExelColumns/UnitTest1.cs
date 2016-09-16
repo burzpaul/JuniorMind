ca54775950de79730d8cdcb7b7ed08d0c6643cb1
Paul Burz <burzpaul@yahoo.ro>
2016-09-16 05:20:12 +0300

    Triple Letter Columns

--- ExelColumns/ExelColumns/UnitTest1.cs
+++ ExelColumns/ExelColumns/UnitTest1.cs
@@ -16,17 +16,23 @@ namespace ExelColumns
         {
             Assert.AreEqual("BA", DetermineExelColumn(53));
         }
+        [TestMethod]
+        public void TripleLetterColumns()
+        {
+            Assert.AreEqual("LKB", DetermineExelColumn(8712));
+        }
         string DetermineExelColumn(int n)
         {
             string[] alphabet = {"","A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
-            int i,j, count=0;
-            for(j = 0; j <= 26; j++)
-                for (i = 1; i <= 26; i++)
-                {
-                    count++;
-                    if (count == n)
-                        return alphabet[j]+alphabet[i];
-                }
+            int i,j,k,count=0;
+            for(k=0; k <= 26; k++)
+                for (j = 0; j <= 26; j++)
+                    for (i = 1; i <= 26; i++)
+                    {
+                        count++;
+                        if (count == n)
+                            return alphabet[k]+alphabet[j]+alphabet[i];
+                    }
             return null;
         }
     }
