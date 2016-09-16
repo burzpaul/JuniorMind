d82e2debb6542b056391cba2cfc31a2d55551f75
Paul Burz <burzpaul@yahoo.ro>
2016-09-16 05:16:54 +0300

    DoubleLetter Columns

--- ExelColumns/ExelColumns/UnitTest1.cs
+++ ExelColumns/ExelColumns/UnitTest1.cs
@@ -11,16 +11,22 @@ namespace ExelColumns
         {
             Assert.AreEqual("E", DetermineExelColumn(5));
         }
+        [TestMethod]
+        public void DoubleLetterColum()
+        {
+            Assert.AreEqual("BA", DetermineExelColumn(53));
+        }
         string DetermineExelColumn(int n)
         {
-            string[] alphabet = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
-            int i, count=0;
-            for (i = 0; i <= 26; i++)
-            {
-                count++;
-                if (count == n)
-                    return alphabet[n - 1];
-            }
+            string[] alphabet = {"","A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+            int i,j, count=0;
+            for(j = 0; j <= 26; j++)
+                for (i = 1; i <= 26; i++)
+                {
+                    count++;
+                    if (count == n)
+                        return alphabet[j]+alphabet[i];
+                }
             return null;
         }
     }
