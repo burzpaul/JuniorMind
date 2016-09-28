35f3efa3be8b50ee3b30740a69fb1e3dbab02216
Paul Burz <burzpaul@yahoo.ro>
2016-09-29 02:32:57 +0300

    Added GetTheIndex and BinaryConversion

--- BinaryNumbers/BinaryNumbers/UnitTest1.cs
+++ BinaryNumbers/BinaryNumbers/UnitTest1.cs
@@ -7,8 +7,42 @@ namespace BinaryNumbers
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GiveDecimalNumber()
         {
+            Assert.AreEqual("110001", BinaryNumbers(49));
         }
+        public string BinaryNumbers(int number)
+        {
+            int index = GetTheIndex(number);
+            byte[] bytes = new byte[index];
+            BinaryConversion(bytes, index-1, number);
+            string response = string.Empty;
+
+            foreach (byte b in bytes)
+                response += (int)b;
+
+                    return response;
+           
+        }
+        public int GetTheIndex(int theNumber)
+        {
+            int m = 0;
+            while(theNumber>0)
+            {
+                theNumber = theNumber / 2;
+                m++;
+            }
+            return m;
+        }
+       void BinaryConversion(byte[] array,int theIndex,int theNumber)
+        {
+            while(theIndex>=0)
+            {
+                array[theIndex] = (byte)(theNumber % 2);
+                theNumber = theNumber / 2;
+                theIndex--;
+            }
+        }       
+
     }
 }
