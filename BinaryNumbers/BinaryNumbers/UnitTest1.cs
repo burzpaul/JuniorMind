4c69c48310fcb49d80bd59ac6203ab4e80f984f2
Paul Burz <burzpaul@yahoo.ro>
2016-10-14 02:37:50 +0300

    RightShift

--- BinaryNumbers/BinaryNumbers/UnitTest1.cs
+++ BinaryNumbers/BinaryNumbers/UnitTest1.cs
@@ -17,22 +17,27 @@ namespace BinaryNumbers
         [TestMethod]
         public void NotOperator()
         {
-            CollectionAssert.AreEqual(TransformToBinary(211), ImplementNotOperator(TransformToBinary(300)));
+            CollectionAssert.AreEqual(TransformToBinary(206), NotOperator(TransformToBinary(49)));
         }
         [TestMethod]
         public void OrOperator()
         {
-            CollectionAssert.AreEqual(TransformToBinary(7), ImplementOperator(TransformToBinary(5), TransformToBinary(3), "OR"));
+            CollectionAssert.AreEqual(TransformToBinary(7), OperatorOperations(TransformToBinary(5), TransformToBinary(3), "OR"));
         }
         [TestMethod]
         public void AndOperator()
         {
-            CollectionAssert.AreEqual(TransformToBinary(1), ImplementOperator(TransformToBinary(5), TransformToBinary(3), "AND"));
+            CollectionAssert.AreEqual(TransformToBinary(1), OperatorOperations(TransformToBinary(5), TransformToBinary(3), "AND"));
         }
         [TestMethod]
         public void XoROperator()
         {
-            CollectionAssert.AreEqual(TransformToBinary(6), ImplementOperator(TransformToBinary(5), TransformToBinary(3), "XOR"));
+            CollectionAssert.AreEqual(TransformToBinary(6), OperatorOperations(TransformToBinary(5), TransformToBinary(3), "XOR"));
+        }
+        [TestMethod]
+        public void RightShift()
+        {
+            CollectionAssert.AreEqual(TransformToBinary(12), RightShift(TransformToBinary(50), 2));
         }
         public byte[] TransformToBinary(int number)
         {
@@ -49,63 +54,61 @@ namespace BinaryNumbers
 
             return resultArray;
         }
-        public byte[] ImplementNotOperator(byte[] byteArray)
+        public byte[] NotOperator(byte[] byteArray)
         {
-            int position = 0;
-
             if (byteArray.Length < 8)
             {
                 Array.Reverse(byteArray);
                 Array.Resize(ref byteArray, byteArray.Length + (8 - byteArray.Length));
                 Array.Reverse(byteArray);
             }
-            foreach (byte b in byteArray)
+            for(int i=0; i < byteArray.Length; i++)
             {
-                byteArray[position] = (byte)((b == 0) ? 1 : 0);
-                position++;
+                byteArray[i] = (byte)((byteArray[i] == 0) ? 1 : 0);
             }
             return TrimArray(byteArray);
         }
-        public byte[] ImplementOperator(byte[] firstArray, byte[] secondArray, string Case)
+        public byte[] OperatorOperations(byte[] firstArray, byte[] secondArray, string selectedOperation)
         {
-            if (firstArray.Length > secondArray.Length)
-            {
-                Array.Reverse(secondArray);
-                Array.Resize(ref secondArray, (secondArray.Length + (firstArray.Length - secondArray.Length)));
-                Array.Reverse(secondArray);
-            }
-            else
-            {
-                Array.Reverse(firstArray);
-                Array.Resize(ref firstArray, (firstArray.Length + (secondArray.Length - firstArray.Length)));
-                Array.Reverse(firstArray);
-            }
-
-            byte[] resultArray = new byte[firstArray.Length];
-            for (int i = 0; i < firstArray.Length; i++)
-                switch (Case)
+            byte[] resultArray = new byte[((firstArray.Length > secondArray.Length) ? firstArray.Length : secondArray.Length)];
+            for (int i = 0; i < resultArray.Length; i++)
+                switch (selectedOperation)
                 {
 
                     case "OR":
-                        resultArray[i] = (byte)((firstArray[i] == 0 && secondArray[i] == 0) ? 0 : 1);
+                        resultArray[resultArray.Length - 1 -i] = (byte)(((GetIndex(firstArray,i) == 0 && (GetIndex(secondArray, i) == 0) ? 0 : 1)));
                         break;
                     case "AND":
-                        resultArray[i] = (byte)((firstArray[i] == 1 && secondArray[i] == 1) ? 1 : 0);
+                        resultArray[resultArray.Length - 1 - i] = (byte)(((GetIndex(firstArray, i) == 1 && (GetIndex(secondArray, i) == 1) ? 1 : 0)));
                         break;
                     case "XOR":
-                        resultArray[i] = (byte)((firstArray[i] == 0 && secondArray[i] == 1 || firstArray[i] == 1 && secondArray[i] == 0) ? 1 : 0);
+                        resultArray[resultArray.Length - 1 - i] = (byte)((GetIndex(firstArray, i) == 0 && GetIndex(secondArray, i) == 1 || GetIndex(firstArray, i) == 1 && GetIndex(secondArray, i) == 0) ? 1 : 0);
                         break;
                 }
 
             return TrimArray(resultArray); ;
         }
+        public byte[] RightShift(byte[] byteArray, int numberOfShitfs)
+        {
+            Array.Resize(ref byteArray, byteArray.Length - numberOfShitfs);
+            return byteArray;
+        }
         public byte[] TrimArray(byte[] byteArray)
         {
             int firstOne = Array.IndexOf(byteArray, (byte)(1));
-            Array.Reverse(byteArray);
-            Array.Resize(ref byteArray, (byteArray.Length - firstOne));
-            Array.Reverse(byteArray);
+         
+                Array.Reverse(byteArray);
+                Array.Resize(ref byteArray, byteArray.Length - firstOne);
+                Array.Reverse(byteArray);
+         
             return byteArray;
+        }
+        byte GetIndex(byte[] Arr, int k)
+        {
+            if (k >= Arr.Length)
+                return 0;
+            return Arr[Arr.Length - 1 - k];
+    
         }
     }
 }
