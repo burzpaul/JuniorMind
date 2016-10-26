5b1d755c1ed036f4ebafa0f01dac35acd617cb08
Paul Burz <burzpaul@yahoo.ro>
2016-10-27 01:48:28 +0300

    Cheapest and Most ExpensiveItems

--- Shopping/Shopping/UnitTest1.cs
+++ Shopping/Shopping/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Shopping
@@ -10,9 +11,15 @@ namespace Shopping
         public void TestMethod1()
         {
             var shoppingItems = new ShoppingCart[] { new ShoppingCart("Tide", 17.32),new ShoppingCart("Soap",3.45),new ShoppingCart("CaptainCrunch",7.99)};
-            double a = 28.76;
+            double expected = 28.76;
             double delta = 1;
-            Assert.AreEqual(a, CalculateTotalCost(shoppingItems), delta);
+            Assert.AreEqual(expected, CalculateTotalCost(shoppingItems), delta);
+        }
+        [TestMethod]
+        public void TheCheapestItem()
+        {
+            var shoppingItems = new ShoppingCart[] { new ShoppingCart("Tide", 17.32), new ShoppingCart("Soap", 3.45), new ShoppingCart("CaptainCrunch", 7.99), new ShoppingCart("BubbleGum", 1.25), new ShoppingCart("AfterShave",3.75)};
+            Assert.AreEqual(shoppingItems[3].productCost, FindItem(shoppingItems,false));
         }
         struct ShoppingCart
         {
@@ -24,7 +31,6 @@ namespace Shopping
                 this.productCost = productCost;
             }
         }
-
         private double CalculateTotalCost(ShoppingCart[] shoppingItems)
         {
             double total = 0;
@@ -32,5 +38,20 @@ namespace Shopping
                 total += shoppingItems[i].productCost;
             return total;
         }
+        private double FindItem(ShoppingCart[] shoppingItems, bool whatItem)
+        {
+            double element = 0;
+            if (whatItem)
+                for (int i = 0; i < shoppingItems.Length; i++)
+                    if (element < shoppingItems[i].productCost)
+                        element = shoppingItems[i].productCost;
+            if (!whatItem)
+                element = shoppingItems[0].productCost;
+                for (int i = 0; i < shoppingItems.Length; i++)
+                    if (element > shoppingItems[i].productCost)
+                        element = shoppingItems[i].productCost;
+            return element;
+        }
+
     }
 }
