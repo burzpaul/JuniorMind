9f451284b7de6ad8def5f27287b1e50a5e69f11c
Paul Burz <burzpaul@yahoo.ro>
2016-10-31 03:41:30 +0200

    Added enum,struct and VerifyAlarm

--- Alarm/Alarm/UnitTest1.cs
+++ Alarm/Alarm/UnitTest1.cs
@@ -4,11 +4,40 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Alarm
 {
     [TestClass]
-    public class UnitTest1
+    public class AlarmProject  
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestTheAlarm()
         {
+            var weekAlarm = new Alarm { hour = 6, day = DayOfWeek.Monday | DayOfWeek.Tuesday | DayOfWeek.Wednesday | DayOfWeek.Thursday | DayOfWeek.Friday };
+            var weekendAlarm = new Alarm { hour = 8, day = DayOfWeek.Saturday | DayOfWeek.Saturday };
+            Assert.AreEqual(true, VerifyAlarm(6, DayOfWeek.Monday));
+
+        }
+        [Flags]
+        enum Days
+        { 
+            Monday = 0x1,
+            Tuesday = 0x2,
+            Wednesday = 0x4,
+            Thursday = 0x8,
+            Friday = 0x10,
+            Saturday = 0x20,
+            Sunday = 0x40
+        }
+        struct Alarm
+        {
+            public DayOfWeek day;
+            public int hour;
+            public Alarm(DayOfWeek day, int hour)
+            {
+                this.day = day;
+                this.hour = hour;
+            }
+        }
+        private bool VerifyAlarm(int v, DayOfWeek monday)
+        {
+            throw new NotImplementedException();
         }
     }
 }
