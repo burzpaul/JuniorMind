fc22249844dffbcf071ec4bb22dec7790e92108e
Paul Burz <burzpaul@yahoo.ro>
2016-11-02 03:26:12 +0200

    Alarm project, working for all Days

--- Alarm/Alarm/UnitTest1.cs
+++ Alarm/Alarm/UnitTest1.cs
@@ -10,16 +10,14 @@ namespace Alarm
         public void TestTheAlarm()
         {
             var weekAlarm = new Alarm { hour = 6, day = DaysOfTheWeek.Tuesday | DaysOfTheWeek.Wednesday | DaysOfTheWeek.Thursday | DaysOfTheWeek.Friday };
-            var weekendAlarm = new Alarm { hour = 8, day = DaysOfTheWeek.Saturday | DaysOfTheWeek.Saturday };
-            Assert.AreEqual(false, VerifyAlarm(6,weekAlarm));//Monday was not set
-            Assert.AreEqual(false, VerifyAlarm(8,weekAlarm));
-            Assert.AreEqual(true, VerifyAlarm(6,weekAlarm));
-            Assert.AreEqual(false, VerifyAlarm(15,weekAlarm));
-            Assert.AreEqual(true, VerifyAlarm(6,weekAlarm));
-            Assert.AreEqual(false, VerifyAlarm(14, weekAlarm));
-            Assert.AreEqual(true, VerifyAlarm(8,weekendAlarm));
-            Assert.AreEqual(false, VerifyAlarm(9,weekendAlarm));
-
+            var weekendAlarm = new Alarm { hour = 8, day = DaysOfTheWeek.Saturday | DaysOfTheWeek.Sunday };
+            Assert.AreEqual(false, VerifyAlarm(6, DaysOfTheWeek.Monday,weekAlarm));//Monday was not set
+            Assert.AreEqual(false, VerifyAlarm(8, DaysOfTheWeek.Tuesday, weekAlarm));
+            Assert.AreEqual(true, VerifyAlarm(6, DaysOfTheWeek.Wednesday, weekAlarm));
+            Assert.AreEqual(false, VerifyAlarm(0, DaysOfTheWeek.Thursday, weekAlarm));
+            Assert.AreEqual(true, VerifyAlarm(6, DaysOfTheWeek.Friday, weekAlarm));
+            Assert.AreEqual(false, VerifyAlarm(0, DaysOfTheWeek.Saturday, weekendAlarm));
+            Assert.AreEqual(true, VerifyAlarm(8, DaysOfTheWeek.Sunday, weekendAlarm));
         }
         [Flags]
         enum DaysOfTheWeek
@@ -31,23 +29,22 @@ namespace Alarm
             Friday = 0x10,
             Saturday = 0x20,
             Sunday = 0x40
-
         }
         struct Alarm
         {
             
-            public int hour;
+            public byte hour;
             public DaysOfTheWeek day;
-            public Alarm( int hour, DaysOfTheWeek day)
+            public Alarm(byte hour, DaysOfTheWeek day)
             {
                 this.day = day;
                 this.hour = hour;
             }
         }
-        private bool VerifyAlarm(int hour,Alarm alarm)
-        {  
-                       if(hour == alarm.hour)
-                            return true;           
+        private bool VerifyAlarm(byte hour,DaysOfTheWeek day,Alarm alarm)
+        {
+            if (((day & alarm.day) != 0) && ((hour & alarm.hour) != 0))
+                return true;           
             return false;
         }
     }
