b4461ac19d9cf4790f0bcca86894b1205cf796ac
Paul Burz <burzpaul@yahoo.ro>
2016-11-08 03:01:04 +0200

    Calculate TotalDistance Refactor

--- Ciclometru/Ciclometru/UnitTest1.cs
+++ Ciclometru/Ciclometru/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Ciclometru
@@ -9,12 +10,21 @@ namespace Ciclometru
         [TestMethod]
         public void TestMethod1()
         {
-            double[] rotationInEverySecondAllCyclists = new double[] { 2, 4, 6, 8, 10, 3, 5, 7, 9, 11, 2, 10, 6, 9, 5 };
+            double[] rotationInEverySecondAllCyclists = new double[] { 2,4,6,8,10, 2,10,6,9,5, 3,5,7,9,11 };
             var cyclist = new Cyclist[] { new Cyclist("Jeremy, Clarkson",10, GetRotationsForEverySecond(rotationInEverySecondAllCyclists,0,4))
-                                          ,new Cyclist("Richard Hammond",11,GetRotationsForEverySecond(rotationInEverySecondAllCyclists,5,9))
-                                          ,new Cyclist("James May",12,GetRotationsForEverySecond(rotationInEverySecondAllCyclists,10,14))};
+                                          ,new Cyclist("James May",12,GetRotationsForEverySecond(rotationInEverySecondAllCyclists,5,9))
+                                          ,new Cyclist("Richard Hammond",11,GetRotationsForEverySecond(rotationInEverySecondAllCyclists,10,14))};
+
             Assert.AreEqual(33.583, CalculateTotalDistance(cyclist),1);
-            Assert.AreEqual("James May second 2" , ReturnTopSpeedSecondAndCyclistsName(cyclist));
+            
+        }
+        private double CalculateTotalDistance(Cyclist[] cyclist)
+        {
+            double distance = 0;
+            for (int i = 0; i < cyclist.Length; i++)
+                for (int j = 0; j < cyclist[i].rotationsForEverySecond.Length; j++)
+                    distance += cyclist[i].rotationsForEverySecond[j] * cyclist[i].diameterInCentimeters * Math.PI;
+            return distance/100;
         }
         struct Cyclist
         {
@@ -27,64 +37,15 @@ namespace Ciclometru
                 this.diameterInCentimeters = diameterInCentimeters;
                 this.rotationsForEverySecond = rotationsForEverySecond;
             }
-        };
-        struct TopSpeedCyclist
-        {
-            public string name;
-            public int second;
-           
-            public TopSpeedCyclist(string name , int second, double speed)
-            {
-                this.name = name;
-                this.second = second;
-            }
         }
-        TopSpeedCyclist getCyclist;
         private double[] GetRotationsForEverySecond(double[] rotationInEverySecondAllCyclists, int lowerBound, int upperBound)
         {
             double[] resultArray = new double[upperBound - lowerBound + 1];
             Array.Copy(rotationInEverySecondAllCyclists, lowerBound, resultArray, 0, resultArray.Length);
             return resultArray;
         }
-        private double CalculateTotalDistance(Cyclist[] cyclist)
-        {
-            double distance = 0;
-            for (int i = 0; i < cyclist.Length; i++)
-                distance += CalculateDistanceForCyclist(cyclist[i]);
-            return distance/100;
-        }
-        private double CalculateDistanceForCyclist(Cyclist cyclist)
-        {
-            double circumference = Math.PI * cyclist.diameterInCentimeters;
-            double distance = 0;
-            for (int i = 0; i < cyclist.rotationsForEverySecond.Length; i++)
-                distance += cyclist.rotationsForEverySecond[i] * circumference;
-            return distance;
-        }
-        private string ReturnTopSpeedSecondAndCyclistsName(Cyclist[] cyclist)
-        {
-            
-            double topSpeed = 0;
-            for (int i = 0; i < cyclist.Length; i++)
-                if (topSpeed < GetTopSpeedForCyclist(cyclist[i]))
-                    topSpeed = GetTopSpeedForCyclist(cyclist[i]);
-            return getCyclist.name + " second " + getCyclist.second  ;
-        }
-        private double GetTopSpeedForCyclist(Cyclist cyclist)
-        {
-            double circumference = Math.PI * cyclist.diameterInCentimeters;
-            double holder = 0;
-            int second = 0;
-            for (int i = 0; i < cyclist.rotationsForEverySecond.Length; i++)
-                if (holder < (circumference * cyclist.rotationsForEverySecond[i]))
-                {
-                    holder = circumference * cyclist.rotationsForEverySecond[i];
-                    second = i + 1;
-                }
-            getCyclist.name = cyclist.name;
-            getCyclist.second = second;
-            return holder;
-        }
+        
+        
     }
 }
 
