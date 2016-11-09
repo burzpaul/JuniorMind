078c3f4c62e33544c1f7bdf53c538c99d7566638
Paul Burz <burzpaul@yahoo.ro>
2016-11-09 05:30:32 +0200

    Refactor and two more tests

--- Sections/Sections/UnitTest1.cs
+++ Sections/Sections/UnitTest1.cs
@@ -7,9 +7,19 @@ namespace Sections
     public class MyClass
     {
         [TestMethod]
-        public void Intersection()
+        public void Intersection1()
         {
-            Assert.AreEqual(new Point(1,0),FindFirstIntersectionPoint(new Point(0, 0),new Directions[] {Directions.right,Directions.right,Directions.up,Directions.left,Directions.down}));
+            Assert.AreEqual(new Point(1, 0), FindFirstIntersectionPoint(new Point(0, 0), new Directions[] { Directions.right, Directions.right, Directions.up, Directions.left, Directions.down }));
+        }
+        [TestMethod]
+        public void Intersection2()
+        {
+            Assert.AreEqual(new Point(4, 4), FindFirstIntersectionPoint(new Point(2, 2), new Directions[] {Directions.up,Directions.right,Directions.up,Directions.right,Directions.right,Directions.down,Directions.right,Directions.down,Directions.left,Directions.left,Directions.up,Directions.up,Directions.up}));
+        }
+        [TestMethod]
+        public void Intersection3()
+        {
+            Assert.AreEqual(new Point(-2, 2), FindFirstIntersectionPoint(new Point(0, 4), new Directions[] { Directions.left,Directions.down,Directions.left,Directions.down,Directions.down,Directions.down,Directions.right,Directions.right,Directions.right,Directions.right,Directions.up,Directions.up,Directions.left,Directions.left,Directions.left,Directions.left,Directions.left,Directions.left }));
         }
         public enum Directions
         {
@@ -41,17 +51,20 @@ namespace Sections
             for(int i=0;i<directions.Length;i++)
             {
                 point.GetDirections(directions[i], 1);
-                storedPoints[i] = point;
-                if (CheckIntersection(point, storedPoints))
-                    return point;
+                        storedPoints[i] = point;    
+            }
+            for (int k = 0; k < storedPoints.Length; k++)
+            {
+                if (CheckIntersection(storedPoints[k], storedPoints,k+1))
+                    return storedPoints[k];
             }
             return new Point(0, 0);
         }
-        private bool CheckIntersection(Point point, Point[] storedPoints)
+        private bool CheckIntersection(Point point, Point[] storedPoints,int position)
         {
-            foreach(var p in storedPoints)
+            for (int i = position; i < storedPoints.Length; i++) 
             {
-                if (point.x == p.x && point.y == p.y)
+                if (point.x == storedPoints[i].x && point.y == storedPoints[i].y)
                     return true;
             }
             return false;
