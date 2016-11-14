507e909996d255fa0b55c44bd4df6093758d69c6
Paul Burz <burzpaul@yahoo.ro>
2016-11-14 09:32:24 +0200

    Fibonacci

--- Fibonnaci/Fibonnaci/UnitTest1.cs
+++ Fibonnaci/Fibonnaci/UnitTest1.cs
@@ -9,19 +9,25 @@ namespace Fibonnaci
         [TestMethod]
         public void FibonacciTest1()
         {
-            Assert.AreEqual(55, Fibonacci(11,0,1));
+            Assert.AreEqual(55, Fibonacci(11));
         }
         [TestMethod]
         public void FibonacciTest2()
         {
-            Assert.AreEqual(0, Fibonacci(1, 0, 1));
+            Assert.AreEqual(0, Fibonacci(1));
         }
         [TestMethod]
         public void FibonacciTest3()
         {
-            Assert.AreEqual(987, Fibonacci(17, 0, 1));
+            Assert.AreEqual(987, Fibonacci(17));
         }
-        public int Fibonacci(int elementNumber,int previous,int current)
+        public int Fibonacci(int n)
+        {
+            int previous = 0;
+            int current = 1;
+            return Fibonacci(n,  previous,  current);
+        }
+        public int Fibonacci(int elementNumber, int previous, int current)
         {
             return elementNumber == 1 ? previous : Fibonacci(elementNumber - 1, current, previous + current);
         }
