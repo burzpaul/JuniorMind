5c98a515e620f0187788273b80f2ba8941b823bc
Paul Burz <burzpaul@yahoo.ro>
2016-11-14 09:17:54 +0200

    Fibonacci

--- Fibonnaci/Fibonnaci/UnitTest1.cs
+++ Fibonnaci/Fibonnaci/UnitTest1.cs
@@ -4,11 +4,29 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Fibonnaci
 {
     [TestClass]
-    public class UnitTest1
+    public class MyClass
     {
         [TestMethod]
-        public void TestMethod1()
+        public void FibonacciTest1()
         {
+            Assert.AreEqual(55, Fibonacci(11,0,1));
+        }
+        [TestMethod]
+        public void FibonacciTest2()
+        {
+            Assert.AreEqual(0, Fibonacci(1, 0, 1));
+        }
+        [TestMethod]
+        public void FibonacciTest3()
+        {
+            Assert.AreEqual(987, Fibonacci(17, 0, 1));
+        }
+        public int Fibonacci(int elementNumber,int previous,int current)
+        {
+            if (elementNumber == 1)
+                return previous;
+            else
+                return Fibonacci(elementNumber - 1, current, previous + current);
         }
     }
 }
