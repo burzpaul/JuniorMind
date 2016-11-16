407271d8114e51daab4688d57abb6b8d815c9969
Paul Burz <burzpaul@yahoo.ro>
2016-11-16 15:20:27 +0200

    Replace Char With String

--- ReplaceCharacterInString/ReplaceCharacterInString/UnitTest1.cs
+++ ReplaceCharacterInString/ReplaceCharacterInString/UnitTest1.cs
@@ -7,8 +7,29 @@ namespace ReplaceCharacterInString
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Test()
         {
+            Assert.AreEqual("Pitbull", ReplaceInString("Pbull","itb",'b'));
+        }
+        [TestMethod]
+        public void Test2()
+        {
+            Assert.AreEqual("abcd", ReplaceInString("x", "abcd", 'x'));
+        }
+        [TestMethod]
+        public void Test3()
+        {
+            Assert.AreEqual("antidisestablishmentarianism", ReplaceInString("antxdxsestablxshmentarxanxsm", "i", 'x'));
+        }
+        public string ReplaceInString(string givenString, string replaceString, char characterToReplace)
+        {
+            if (givenString.Length > 0)
+            {
+                string temporary = ReplaceInString(givenString.Substring(1, givenString.Length - 1), replaceString, characterToReplace);
+                if (givenString[0] == characterToReplace) return replaceString + temporary;
+                return givenString[0] + temporary;
+            }
+            return givenString;
         }
     }
 }
