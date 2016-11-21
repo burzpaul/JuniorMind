d30dadb58919676c328c55c542bc69e1e1e396b9
Paul Burz <burzpaul@yahoo.ro>
2016-11-21 23:28:33 +0200

    Added Calculate function

--- Calculator/Calculator/UnitTest1.cs
+++ Calculator/Calculator/UnitTest1.cs
@@ -7,8 +7,13 @@ namespace Calculator
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Test1()
         {
+            Assert.AreEqual(12, Calculate("* 3 4 "),1);
+        }
+        public double Calculate(string operationsAndNumbers)
+        {
+            return 0;
         }
     }
 }
