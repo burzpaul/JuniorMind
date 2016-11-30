3134159760747cc0bc00cbad50f4a96a6b758d68
Paul Burz <burzpaul@yahoo.ro>
2016-11-30 03:22:04 +0200

    Calculator Project

--- Calculator/Calculator/UnitTest1.cs
+++ Calculator/Calculator/UnitTest1.cs
@@ -9,11 +9,38 @@ namespace Calculator
         [TestMethod]
         public void Test1()
         {
-            Assert.AreEqual(12, Calculate("* 3 4 "),1);
+            
+            Assert.AreEqual(12, Calculate("* 3 4"),1);
         }
-        public double Calculate(string operationsAndNumbers)
+        [TestMethod]
+        public void Test2()
         {
-            return 0;
+            Assert.AreEqual(1549.41,Calculate("+ / * + 56 45 46 3 - 1 0.25"),1);
+        }
+        double Calculate(string s)
+        {
+            string[] elements = s.Split(' ');
+            int i = 0;
+            return Calculate(elements, ref i);
+        }
+
+        double Calculate(string[] elements, ref int i)
+        {
+            string currentElement = elements[i++];
+            double result = 0;
+            if (double.TryParse(currentElement, out result))
+            {
+                return result;
+            }
+
+            switch (currentElement)
+            {
+                case "+": return Calculate(elements, ref i) + Calculate(elements, ref i);
+                case "-": return Calculate(elements, ref i) - Calculate(elements, ref i);
+                case "*": return Calculate(elements, ref i) * Calculate(elements, ref i);
+                default: return Calculate(elements, ref i) / Calculate(elements, ref i);
+            }
         }
     }
 }
+
