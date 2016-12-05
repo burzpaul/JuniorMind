e66b1cc3e319b98f6f428d67315c41edf7431e0d
Paul Burz <burzpaul@yahoo.ro>
2016-12-05 07:06:02 +0200

    Pascal's Triangle

--- PascalsTriangle/PascalsTriangle/UnitTest1.cs
+++ PascalsTriangle/PascalsTriangle/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace PascalsTriangle
@@ -7,8 +8,30 @@ namespace PascalsTriangle
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Test1()
         {
+            CollectionAssert.AreEqual(new int[] { 1, 1}, GenereteRowOfPascalsTriangle(2));     
+        }
+        [TestMethod]
+        public void Test2()
+        {
+            CollectionAssert.AreEqual(new int[] { 1, 2, 1 }, GenereteRowOfPascalsTriangle(3));
+        }
+        [TestMethod]
+        public void Test3()
+        {
+            CollectionAssert.AreEqual(new int[] { 1, 14, 91, 364, 1001, 2002, 3003, 3432, 3003, 2002, 1001, 364, 91, 14, 1 }, GenereteRowOfPascalsTriangle(15));
+        }
+        private int[] GenereteRowOfPascalsTriangle(int row)
+        {
+            int[] result = new int[row];
+            result[0] = 1;
+            result[result.Length - 1] = 1;
+            if (row < 3) return result;
+            int[] previousRow = GenereteRowOfPascalsTriangle(row - 1);
+            for (int i = 1; i < row - 1; i++)
+                result[i] = previousRow[i - 1] + previousRow[i];
+            return result;
         }
     }
 }
