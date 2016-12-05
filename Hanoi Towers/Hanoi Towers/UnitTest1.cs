c972d574f1c54461ab33ddbbcec7b904e9b5f726
Paul Burz <burzpaul@yahoo.ro>
2016-12-05 05:32:29 +0200

    Hanoi Towers

--- Hanoi Towers/Hanoi Towers/UnitTest1.cs
+++ Hanoi Towers/Hanoi Towers/UnitTest1.cs
@@ -9,11 +9,27 @@ namespace Hanoi_Towers
         [TestMethod]
         public void TestMethod1()
         {
-            CollectionAssert.AreEqual(new int[] { 5, 4, 3, 2, 1 },HanoiTowers(5, new int[] { 5, 4, 3, 2, 1 },new int[63],new int[63]));
+            CollectionAssert.AreEqual(new int[] { 5, 4, 3, 2, 1 }, HanoiTowers(5, new int[] { 5, 4, 3, 2, 1 }, new int[5], new int[5]));
         }
         public int[] HanoiTowers(int disks, int[] A, int[] B, int[] C)
         {
-            return A;
+            if (disks == 1) 
+            {
+                return  MoveDisks(disks, A, C);
+            }
+            else
+            {
+                HanoiTowers(disks - 1, A, B, C);
+                MoveDisks(disks, A, C);
+                HanoiTowers(disks - 1, B, C, A);
+            }
+            return C;
+        }
+        int[] MoveDisks(int numberOfDisks, int[] fromTower, int[] toTower)
+        {
+            toTower[numberOfDisks - 1] = fromTower[numberOfDisks - 1];
+            Array.Resize(ref fromTower, fromTower.Length - 1);
+            return fromTower;
         }
     }
 }
