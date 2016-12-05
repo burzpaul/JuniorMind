897c5e792a7d9839c228ffd24fd57ba7b8031124
Paul Burz <burzpaul@yahoo.ro>
2016-12-05 05:27:36 +0200

    Added HanoiTowers function

--- Hanoi Towers/Hanoi Towers/UnitTest1.cs
+++ Hanoi Towers/Hanoi Towers/UnitTest1.cs
@@ -9,6 +9,11 @@ namespace Hanoi_Towers
         [TestMethod]
         public void TestMethod1()
         {
+            CollectionAssert.AreEqual(new int[] { 5, 4, 3, 2, 1 },HanoiTowers(5, new int[] { 5, 4, 3, 2, 1 },new int[63],new int[63]));
+        }
+        public int[] HanoiTowers(int disks, int[] A, int[] B, int[] C)
+        {
+            return A;
         }
     }
 }
