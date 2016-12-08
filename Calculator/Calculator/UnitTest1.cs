0eab8df540451b67fc89bb29696290885d20ca63
Paul Burz <burzpaul@yahoo.ro>
2016-12-08 11:22:20 +0200

    In Order Calculator

--- Calculator/Calculator/UnitTest1.cs
+++ Calculator/Calculator/UnitTest1.cs
@@ -9,23 +9,17 @@ namespace Calculator
         [TestMethod]
         public void Test1()
         {
-            
-            Assert.AreEqual(12, Calculate("* 3 4"),1);
+            Assert.AreEqual(8,Calculate(" "),1);  
         }
-        [TestMethod]
-        public void Test2()
+        double Calculate(string input)
         {
-            Assert.AreEqual(1549.41,Calculate("+ / * + 56 45 46 3 - 1 0.25"),1);
-        }
-        double Calculate(string s)
-        {
-            string[] elements = s.Split(' ');
+            string[] elements = input.Split(' ');
             int i = 0;
             return Calculate(elements, ref i);
         }
-
-        double Calculate(string[] elements, ref int i)
+        private double Calculate(string[] elements, ref int i)
         {
+            double previous = Calculate(elements, ref i);
             string currentElement = elements[i++];
             double result = 0;
             if (double.TryParse(currentElement, out result))
@@ -34,12 +28,11 @@ namespace Calculator
             }
             switch (currentElement)
             {
-                case "+": return Calculate(elements, ref i) + Calculate(elements, ref i);
-                case "-": return Calculate(elements, ref i) - Calculate(elements, ref i);
-                case "*": return Calculate(elements, ref i) * Calculate(elements, ref i);
-                default: return Calculate(elements, ref i) / Calculate(elements, ref i);
+                case "+": return previous + Calculate(elements, ref i);
+                case "-": return previous - Calculate(elements, ref i);
+                case "*": return previous * Calculate(elements, ref i);
+                default: return previous / Calculate(elements, ref i);
             }
         }
     }
 }
-
---
+++ InOrderCalculator/InOrderCalculator/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InOrderCalculator
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(6, Calculate("3 + 2 + 1"), 1);
+        }
+        public double Calculate(string input)
+        {
+            string[] elements = input.Split(' ');
+            return Calculate(elements,0);
+        }
+        private double Calculate(string[] elements, int i)
+        {
+            double result = 0;
+            if (i < elements.Length - 2) 
+            {
+                string currentElement = elements[i + 1];
+                result = Convert.ToDouble(elements[i]);
+                switch (currentElement)
+                {
+                    case "+": return result + Calculate(elements, i + 2);
+                    case "-": return result - Calculate(elements, i + 2);
+                    case "*": return result * Calculate(elements, i + 2);
+                    default: return result / Calculate(elements, i + 2);
+                }
+            }
+            return result;
+        }
+    }
+}
