15ba717562e4201e3a112a90f3337fe408da5d61
Paul Burz <burzpaul@yahoo.ro>
2017-04-06 01:10:53 +0300

    Added new Project InOrderCalculatorV2

---
+++ InOrderCalculatorV2/InOrderCalculatorV2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("InOrderCalculatorV2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("InOrderCalculatorV2")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d8bc6cd6-f2f3-4c8a-9c54-d1d4867c3e89")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
