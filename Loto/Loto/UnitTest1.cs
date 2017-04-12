c5b04e0fbf81795e12ba9b6834fa3f58057a2017
Paul Burz <burzpaul@yahoo.ro>
2017-04-12 10:32:29 +0300

    LotoNumbers with Insertion Sort

--- Loto/Loto/UnitTest1.cs
+++ Loto/Loto/UnitTest1.cs
@@ -7,8 +7,28 @@ namespace Loto
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestForSotedLotoNumbers()
         {
+            int[] sorted = new int[] { 1, 3, 57, 65, 79, 98, 123 };
+            CollectionAssert.AreEqual(sorted, SortLotoNumbers(new int[] { 65, 1, 123, 3, 57, 98, 79 }));
+        }
+        public int[] SortLotoNumbers(int[] numbersArray)
+        {
+            for (int i = 0; i < numbersArray.Length - 1; i++) 
+            {
+                int j = i + 1;
+                while (j > 0)
+                {
+                    if (numbersArray[j - 1] > numbersArray[j])
+                    {
+                        int temp = numbersArray[j - 1];
+                        numbersArray[j - 1] = numbersArray[j];
+                        numbersArray[j] = temp;
+                    }
+                    j--;
+                }
+            }
+            return numbersArray;
         }
     }
 }
