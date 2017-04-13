28ca80e93b679bfd70bc2058c6198ae4b9e29871
Paul Burz <burzpaul@yahoo.ro>
2017-04-13 17:17:35 +0300

    .

--- RepairShop/RepairShop/UnitTest1.cs
+++ RepairShop/RepairShop/UnitTest1.cs
@@ -21,14 +21,14 @@ namespace RepairShop
             }
         }
         [TestMethod]
-        public void TestMethod1()
+        public void TestForAplication()
         {
             var ordersInfo = new OrdersInfo[] { new OrdersInfo(3, "Engine", PriorityLevels.High),
                                                 new OrdersInfo(5,"Direction",PriorityLevels.High),
                                                 new OrdersInfo( 1, "Wheels", PriorityLevels.Medium),
                                                 new OrdersInfo( 4, "FrontWindows", PriorityLevels.Medium),
-                                                new OrdersInfo( 6, "Seat", PriorityLevels.Low),
-                                                new OrdersInfo( 2, "Mirrors", PriorityLevels.Low)};
+                                                new OrdersInfo( 2, "Mirrors", PriorityLevels.Low),
+                                                new OrdersInfo( 6, "Seat", PriorityLevels.Low),};
 
             var unorganizedOrders = new OrdersInfo[] { new OrdersInfo(1, "Wheels", PriorityLevels.Medium),
                                                 new OrdersInfo(2,"Mirrors",PriorityLevels.Low),
@@ -36,56 +36,67 @@ namespace RepairShop
                                                 new OrdersInfo( 4, "FrontWindows", PriorityLevels.Medium),
                                                 new OrdersInfo( 5, "Direction", PriorityLevels.High),
                                                 new OrdersInfo( 6, "Seat", PriorityLevels.Low)};
+            var a = OrderTheOrders(unorganizedOrders);
 
-            Assert.AreEqual(ordersInfo, OrderTheOrders(unorganizedOrders));
+            CollectionAssert.AreEqual(ordersInfo, a);
         }
         [TestMethod]
-        public void MyTestMethod()
+        public void TestFor_When_Both_Have_EqualPriorityLevelButDifferentOrderNumber()
         {
-            var a = new OrdersInfo(2, "Mirrors", PriorityLevels.Low);
-            var b = new OrdersInfo(3, "Engine", PriorityLevels.High);
-            Swap(ref b, ref a);
-            Assert.AreEqual(b.orderProblem, "Mirrors");
+            var order1 = new OrdersInfo[] { new OrdersInfo(2, "Heat", PriorityLevels.High), new OrdersInfo(1, "Wheels", PriorityLevels.High) };
+            var order2 = new OrdersInfo(1, "Wheels", PriorityLevels.High);
+            var actual = OrderTheOrders(order1)[0];
+            Assert.AreEqual(order2.orderNumber, actual.orderNumber);
+        }
+        [TestMethod]
+        public void TestForSwap()
+        {
+            var firstOrder = new OrdersInfo(2, "Mirrors", PriorityLevels.Low);
+            var secondOrder = new OrdersInfo(3, "Engine", PriorityLevels.High);
+            Swap(ref secondOrder, ref firstOrder);
+            Assert.AreEqual(secondOrder.orderProblem, "Mirrors");
         }
         private OrdersInfo[] OrderTheOrders(OrdersInfo[] ordersInformation)
         {
-            QuickSort(ordersInformation, 0, ordersInformation.Length - 1);
+            QuickSort3(ordersInformation, 0, ordersInformation.Length - 1);
             return ordersInformation;
-        }
+        } 
+            private void QuickSort3(OrdersInfo[] ordersInformation, int start, int end)
+            {
+                if (end < start) return;
+                int left = start;
+                int right = end;
+                var pivot = ordersInformation[start];
+                int index = start;
+                while (index < right)
+            {
+                if (LessThen(pivot, ordersInformation[index]))
+                {
+                    Swap(ref ordersInformation[left++], ref ordersInformation[index++]);
+                }
+                else if (LessThen(ordersInformation[index], pivot)) 
+                {
+                    Swap(ref ordersInformation[index], ref ordersInformation[right--]);
+                }
+                else
+                {
+                    index++;
+                }
+            }
+            QuickSort3(ordersInformation, start, left - 1);
+            QuickSort3(ordersInformation, right + 1, end);
+            }
 
-        private void QuickSort(OrdersInfo[] ordersInformation, int left, int right)
+        private static bool LessThen(OrdersInfo a, OrdersInfo b)
         {
-            if (right <= left)
-                return;
-            if(ordersInformation[right].priorityLevel < ordersInformation[left].priorityLevel)
-            {
-                Swap(ref ordersInformation[left], ref ordersInformation[right]);
-            }
-            int i = left + 1;
-            int j = right - 1;
-            int k = left + 1;
-            
-            while(i <= j )
-            {
-                if (ordersInformation[i].priorityLevel < ordersInformation[left].priorityLevel)
-                    Swap(ref ordersInformation[i++], ref ordersInformation[k++]);
-                else if (ordersInformation[right].priorityLevel < ordersInformation[j].priorityLevel)
-                    Swap(ref ordersInformation[k], ref ordersInformation[j--]);
-                else k++;
-            }
-            Swap(ref ordersInformation[left], ref ordersInformation[--i]);
-            Swap(ref ordersInformation[right],ref ordersInformation[++j]);
-
-            QuickSort(ordersInformation, left, i - 1);
-            if (ordersInformation[i].priorityLevel < ordersInformation[j].priorityLevel)
-                QuickSort(ordersInformation, i++, j--);
-            QuickSort(ordersInformation, j++, right);
+            if (a.priorityLevel == b.priorityLevel)
+                return a.orderNumber < b.orderNumber;
+            return a.priorityLevel < b.priorityLevel;
         }
 
         private void Swap(ref OrdersInfo ordersInfo1, ref OrdersInfo ordersInfo2)
         {
-            var temp = new OrdersInfo();
-            temp = ordersInfo1;
+            var temp = ordersInfo1;
             ordersInfo1 = ordersInfo2;
             ordersInfo2 = temp;
         }
