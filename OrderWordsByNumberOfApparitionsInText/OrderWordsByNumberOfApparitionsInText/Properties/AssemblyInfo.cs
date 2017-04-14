df4d92df56b0495eb8c166d9f446d12df6e22fad
Paul Burz <burzpaul@yahoo.ro>
2017-04-14 10:51:54 +0300

    Add empy OrdeWordsByNumberOfApparitionsInText project

---
+++ OrderWordsByNumberOfApparitionsInText/OrderWordsByNumberOfApparitionsInText/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OrderWordsByNumberOfApparitionsInText")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OrderWordsByNumberOfApparitionsInText")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("66d9423b-c7b8-48d3-8dca-f1d43a3cf07a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
