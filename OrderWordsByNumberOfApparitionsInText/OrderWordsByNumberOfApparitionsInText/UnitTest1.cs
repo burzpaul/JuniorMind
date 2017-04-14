f10f8b6fc4086b084327a94df8be6b81a353ac96
Paul Burz <burzpaul@yahoo.ro>
2017-04-14 15:50:15 +0300

    Refactor Project

--- OrderWordsByNumberOfApparitionsInText/OrderWordsByNumberOfApparitionsInText/UnitTest1.cs
+++ OrderWordsByNumberOfApparitionsInText/OrderWordsByNumberOfApparitionsInText/UnitTest1.cs
@@ -6,12 +6,12 @@ namespace OrderWordsByNumberOfApparitionsInText
     [TestClass]
     public class UnitTest1
     {
-        public struct Words
+        public struct Word
         {
             public string theWord;
             public int numberOfApparitions;
 
-            public Words(string theWord, int numberOfApparitions)
+            public Word(string theWord, int numberOfApparitions)
             {
                 this.theWord = theWord;
                 this.numberOfApparitions = numberOfApparitions;
@@ -22,49 +22,68 @@ namespace OrderWordsByNumberOfApparitionsInText
         {
             string text = "word word word word pc pc pc germany germany one one one two two";
 
-            var given = new Words[] { new Words("pc", 0), new Words("word", 0), new Words("germany", 0),
-                                        new Words("two", 0), new Words("one", 0) };
+            var expected = new Word[] { new Word("two", 2), new Word("germany", 2), new Word("one", 3),
+                                        new Word("pc", 3), new Word("word", 4) };
 
-            var expected = new Words[] { new Words("two", 2), new Words("germany", 2), new Words("pc", 3),
-                                        new Words("one", 3), new Words("word", 4) };
-
-            CollectionAssert.AreEqual(expected, OrderTheWordsByNumberOfApparitions(given, text));
+            CollectionAssert.AreEqual(expected, OrderTheWordsByNumberOfAppearances(text));
         }
         [TestMethod]
         public void Test_The_Number_Apparitions_Of_Key_Words2()
         {
             string text = "abc abc abc CCC CCC oOo";
 
-            var given = new Words[] { new Words("abc", 0), new Words("ccc", 0), new Words("ooo", 0) };
+            var expected = new Word[] { new Word("ooo", 1), new Word("ccc", 2), new Word("abc", 3) };
 
-            var expected = new Words[] { new Words("ooo", 1), new Words("ccc", 2), new Words("abc", 3) };
-
-
-            CollectionAssert.AreEqual(expected, OrderTheWordsByNumberOfApparitions(given, text));
+            CollectionAssert.AreEqual(expected, OrderTheWordsByNumberOfAppearances(text));
         }
-        public Words[] OrderTheWordsByNumberOfApparitions(Words[] words, string text)
+
+        public Word[] OrderTheWordsByNumberOfAppearances(string text)
         {
-            SearchForWord(words, text.Split(' '));
+            var words = SearchForWord(text.Split(' '));
             HeapSort(words);
             return words;
         }
-        private void SearchForWord(Words[] words, string[] textWords)
+
+        private Word[] SearchForWord(string[] textWords)
         {
-            for (int i = 0; i < words.Length; i++)
+            var words = new Word[] { };
+            foreach (var currentWord in textWords)
             {
-                var currentWord = words[i].theWord;
-                for (int index = 0; index < textWords.Length; index++)
+                var index = FindWordIndex(words, currentWord);
+                if (index != -1)
                 {
-                    var currentTextWord = textWords[index].ToLower();
-                    if (CompareWords(currentTextWord, currentWord))  
-                    {
-                        words[i].numberOfApparitions++;
-                    }
+                    IncreaseAppearances(ref words[index]);
+                    continue;
                 }
+                AddWord(ref words, currentWord);
             }
+            return words;
         }
 
-        private void HeapSort(Words[] words)
+        private void IncreaseAppearances(ref Word word)
+        {
+            word.numberOfApparitions++;
+        }
+
+        private int FindWordIndex(Word[] words, string currentWord)
+        {
+            for (int index = 0; index < words.Length; index++)
+            {
+                if (CompareWords(words[index].theWord, currentWord))
+                {
+                    return index;
+                }
+            }
+            return -1;
+        }
+
+        private void AddWord(ref Word[] words, string textWordNotIndexed)
+        {
+            Array.Resize(ref words, words.Length + 1);
+            words[words.Length - 1] = new Word(textWordNotIndexed.ToLowerInvariant(), 1);
+        }
+
+        private void HeapSort(Word[] words)
         {
             int n = words.Length;
             for (int i = n / 2 - 1; i >= 0; i--)
@@ -73,19 +92,19 @@ namespace OrderWordsByNumberOfApparitionsInText
             }
             for (int i = n - 1; i >= 0; i--)
             {
-                Swap(ref words[0],ref words[i]);
+                Swap(ref words[0], ref words[i]);
 
                 Heapify(words, i, 0);
             }
         }
 
-        private void Heapify(Words[] words, int n, int root)
+        private void Heapify(Word[] words, int n, int root)
         {
             int largest = root;
             int left = 2 * root + 1;
             int right = 2 * root + 2;
 
-            if(left < n && words[left].numberOfApparitions > words[largest].numberOfApparitions)
+            if (left < n && words[left].numberOfApparitions > words[largest].numberOfApparitions)
             {
                 largest = left;
             }
@@ -93,17 +112,16 @@ namespace OrderWordsByNumberOfApparitionsInText
             {
                 largest = right;
             }
-
-            if(largest != root)
+            if (largest != root)
             {
                 Swap(ref words[root], ref words[largest]);
                 Heapify(words, n, largest);
             }
         }
 
-        private void Swap(ref Words firstWord, ref Words words2)
+        private void Swap(ref Word firstWord, ref Word words2)
         {
-            var temp = new Words();
+            var temp = new Word();
             temp = firstWord;
             firstWord = words2;
             words2 = temp;
@@ -111,7 +129,7 @@ namespace OrderWordsByNumberOfApparitionsInText
 
         private bool CompareWords(string firstWord, string secondWord)
         {
-            return firstWord == secondWord;
+            return string.Equals(firstWord, secondWord, StringComparison.InvariantCultureIgnoreCase);
         }
     }
 }
