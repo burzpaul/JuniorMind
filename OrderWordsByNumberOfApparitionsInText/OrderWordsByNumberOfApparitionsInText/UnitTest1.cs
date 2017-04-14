59638bc1cd22e650dd2c52a9579b4c5fdd74c462
Paul Burz <burzpaul@yahoo.ro>
2017-04-14 11:42:38 +0300

    Added SearchForWords function and Test for it

--- OrderWordsByNumberOfApparitionsInText/OrderWordsByNumberOfApparitionsInText/UnitTest1.cs
+++ OrderWordsByNumberOfApparitionsInText/OrderWordsByNumberOfApparitionsInText/UnitTest1.cs
@@ -8,16 +8,16 @@ namespace OrderWordsByNumberOfApparitionsInText
     {
         struct Words
         {
-            string word;
-            int numberOfApparitions;
-            public Words(string word, int numberOfApparitions)
+            public string theWord;
+            public int numberOfApparitions;
+            public Words(string theWord, int numberOfApparitions)
             {
-                this.word = word;
+                this.theWord = theWord;
                 this.numberOfApparitions = numberOfApparitions;
             }
         }
         [TestMethod]
-        public void Test_The_Number_Apparitions_Of_Key_Words_()
+        public void Test_The_Number_Apparitions_Of_Key_Words()
         {
             string text = "word word word word pc pc pc germany germany one one one two two";
 
@@ -29,9 +29,43 @@ namespace OrderWordsByNumberOfApparitionsInText
 
             CollectionAssert.AreEqual(expected, OrderTheWordsByNumberOfApparitions(given, text));
         }
+        [TestMethod]
+        public void TestForSearchWords()
+        {
+            string text = "abc abc abc CCC CCC oOo";
+
+            var given = new Words[] { new Words("abc", 0), new Words("ccc", 0), new Words("ooo", 0) };
+
+            var expected = new Words[] { new Words("abc", 3), new Words("ccc", 2), new Words("ooo", 1) };
+
+            var verifier = SearchForWord(given, text.Split(' '));
+
+            CollectionAssert.AreEqual(expected, verifier);
+        }
         private Words[] OrderTheWordsByNumberOfApparitions(Words[] words, string text)
         {
+            SearchForWord(words, text.Split(' '));
             return null;
+        }
+        private Words[] SearchForWord(Words[] word, string[] textWords)
+        {
+            for (int i = 0; i < word.Length; i++)
+            {
+                var currentWord = word[i].theWord;
+                for (int index = 0; index < textWords.Length; index++)
+                {
+                    var currentTextWord = textWords[index].ToLower();
+                    if (CompareWords(currentTextWord, currentWord))  
+                    {
+                        word[i].numberOfApparitions++;
+                    }
+                }
+            }
+            return word;
+        }
+        private bool CompareWords(string firstWord, string secondWord)
+        {
+            return firstWord == secondWord;
         }
     }
 }
