edd3a0495de79049968fa1788ea4a4483458885b
Paul Burz <burzpaul@yahoo.ro>
2017-04-18 17:31:45 +0300

    HeapSort Class Working

--- OrderWordsByNumberOfApparitionsInText/OrderWordsByNumberOfApparitionsInText/HeapSort.cs
+++ OrderWordsByNumberOfApparitionsInText/OrderWordsByNumberOfApparitionsInText/HeapSort.cs
@@ -4,6 +4,48 @@ namespace OrderWordsByNumberOfApparitionsInText
 {
     public class HeapSort
     {
-        private int heapSize;
+        
+        public void HeapSorting(Word[] words)
+        {
+            int n = words.Length;
+            for (int i = n / 2 - 1; i >= 0; i--)
+            {
+                Heapify(words, n, i);
+            }
+            for (int i = n - 1; i >= 0; i--)
+            {
+                Swap(ref words[0], ref words[i]);
+
+                Heapify(words, i, 0);
+            }
+        }
+
+        public void Heapify(Word[] words, int n, int root)
+        {
+            int largest = root;
+            int left = 2 * root + 1;
+            int right = 2 * root + 2;
+
+            if (left < n && words[left].IsMoreCommon(words[largest]))
+            {
+                largest = left;
+            }
+            if (right < n && words[right].IsMoreCommon(words[largest]))
+            {
+                largest = right;
+            }
+            if (largest != root)
+            {
+                Swap(ref words[root], ref words[largest]);
+                Heapify(words, n, largest);
+            }
+        }
+
+        public void Swap(ref Word firstWord, ref Word words2)
+        {
+            var temp = firstWord;
+            firstWord = words2;
+            words2 = temp;
+        }
     }
 }
--- OrderWordsByNumberOfApparitionsInText/OrderWordsByNumberOfApparitionsInText/MyClass.cs
+++ OrderWordsByNumberOfApparitionsInText/OrderWordsByNumberOfApparitionsInText/MyClass.cs
@@ -9,7 +9,8 @@ namespace OrderWordsByNumberOfApparitionsInText
         public Word[] OrderTheWordsByNumberOfAppearances(string text)
         {
             var words = SearchForWord(text.Split(' '));
-            HeapSort(words);
+            HeapSort heapSort = new HeapSort();
+            heapSort.HeapSorting(words);
             return words;
         }
 
@@ -51,49 +52,6 @@ namespace OrderWordsByNumberOfApparitionsInText
             Array.Resize(ref words, words.Length + 1);
             words[words.Length - 1] = new Word(textWordNotIndexed);
             words[words.Length - 1].IncreaseApparition();
-        }
-
-        public void Swap(ref Word firstWord, ref Word words2)
-        {        
-            var temp = firstWord;
-            firstWord = words2;
-            words2 = temp;
-        }
-
-        public void HeapSort(Word[] words)
-        {
-            int n = words.Length;
-            for (int i = n / 2 - 1; i >= 0; i--)
-            {
-                Heapify(words, n, i);
-            }
-            for (int i = n - 1; i >= 0; i--)
-            {
-                Swap(ref words[0], ref words[i]);
-
-                Heapify(words, i, 0);
-            }
-        }
-
-        public void Heapify(Word[] words, int n, int root)
-        {
-            int largest = root;
-            int left = 2 * root + 1;
-            int right = 2 * root + 2;
-
-            if (left < n && words[left].IsMoreCommon(words[largest]))
-            {
-                largest = left;
-            }
-            if (right < n && words[right].IsMoreCommon(words[largest])) 
-            {
-                largest = right;
-            }
-            if (largest != root)
-            {
-                Swap(ref words[root], ref words[largest]);
-                Heapify(words, n, largest);
-            }
         }
     }
 }
