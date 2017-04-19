18908d660988d022b59ad9d1b33cb53d72c6a41d
Paul Burz <burzpaul@yahoo.ro>
2017-04-19 15:25:43 +0300

    Added Test2IfSorted

--- OrderWordsByNumberOfApparitionsInText/OrderWordsByNumberOfApparitionsInText/SortByMostCommonTest.cs
+++ OrderWordsByNumberOfApparitionsInText/OrderWordsByNumberOfApparitionsInText/SortByMostCommonTest.cs
@@ -19,5 +19,19 @@ namespace OrderWordsByNumberOfApparitionsInText
 
             Assert.AreEqual(true, actual[0].EqualWords(expected[0], false));
         }
+
+        [TestMethod]
+        public void Test2IfSorted()
+        {
+            var words = new Word[] { new Word("third", 10), new Word("first", 4),
+                                        new Word("fourth", 4), new Word("second", 2) };
+
+            var expected = new Word[] { new Word("second", 2), new Word("first", 4),
+                                        new Word("fourth", 4),new Word("third", 10)};
+
+            var actual = new MostCommonWords(words).Sort();
+
+            Assert.AreEqual(true, actual[0].EqualWords(expected[0], false));
+        }
     }
 }
