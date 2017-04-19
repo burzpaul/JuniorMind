4c6802420b7c27da8af22ece0ab0c2c7b26d459b
Paul Burz <burzpaul@yahoo.ro>
2017-04-19 09:25:36 +0300

    Add TestIfSorted

--- OrderWordsByNumberOfApparitionsInText/OrderWordsByNumberOfApparitionsInText/MostCommonWordsTest.cs
+++ OrderWordsByNumberOfApparitionsInText/OrderWordsByNumberOfApparitionsInText/MostCommonWordsTest.cs
@@ -6,6 +6,55 @@ namespace OrderWordsByNumberOfApparitionsInText
     [TestClass]
     public class MostCommonWordsTest
     {
-        
+        [TestMethod]
+        public void TestIfSorted()
+        {
+            var words = new Word[] { new Word("third", 3), new Word("first", 1),
+                                        new Word("fourth", 4), new Word("second", 2) };
+
+            var expected = new Word[] { new Word("first", 1), new Word("second", 2),
+                                        new Word("third", 3),new Word("fourth", 4),};
+
+            var sort = new SortByMostCommon();
+            sort.HeapSorting(words);
+
+            Assert.AreEqual(0, CompareWords(expected, words) +
+                CompareWordsByNumberOfApparitions(expected, words));
+        }
+
+        private int CompareWords(Word[] expected, Word[] actual)
+        {
+            int ok = 0;
+            for (int i = 0; i < expected.Length; i++)
+            {
+                if (IsEqualTo(expected[i], actual[i]) == false)
+                {
+                    ok = 1;
+                    break;
+                }
+            }
+            return ok;
+        }
+        private int CompareWordsByNumberOfApparitions(Word[] expected, Word[] actual)
+        {
+            int ok = 0;
+            for (int i = 0; i < expected.Length; i++)
+            {
+                if (IsNumberOfApparitionsEqualTo(expected[i], actual[i]) == false)
+                {
+                    ok = 1;
+                    break;
+                }
+            }
+            return ok;
+        }
+        bool IsEqualTo(Word firstWord, Word secondWord)
+        {
+            return firstWord.EqualWords(secondWord);
+        }
+        bool IsNumberOfApparitionsEqualTo(Word firstWord, Word secondWord)
+        {
+            return firstWord.EqualApparitions(secondWord);
+        }
     }
 }
