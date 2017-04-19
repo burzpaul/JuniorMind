2f690f3f51cd5ee7ffe4c3fc2c92a12ee345f764
Paul Burz <burzpaul@yahoo.ro>
2017-04-19 08:39:40 +0300

    Add Test Test_For_Same_Word_But_Different_Numbers_Of_Apparitions

--- OrderWordsByNumberOfApparitionsInText/OrderWordsByNumberOfApparitionsInText/WordTests.cs
+++ OrderWordsByNumberOfApparitionsInText/OrderWordsByNumberOfApparitionsInText/WordTests.cs
@@ -12,10 +12,18 @@ namespace OrderWordsByNumberOfApparitionsInText
             Assert.AreEqual(true,word.EqualWords("abc"));
         }
         [TestMethod]
-        public void Test_For_Word_Functon_With_Number_Of_Apparitions()
+        public void Test_For_Word_Functon_With_Number_Of_Apparitions_Test_Number_Of_Apparitions()
         {
             var word = new Word("abc", 2);
             Assert.AreEqual(true, word.EqualApparitions(2));
+        }
+        [TestMethod]
+        public void Test_For_Same_Word_But_Different_Numbers_Of_Apparitions()
+        {
+            var word1 = new Word("same", 1);
+            var word2 = new Word("same", 2);
+            if (word1.EqualWords(word2))
+                Assert.AreEqual(true, word2.IsMoreCommon(word1));
         }
     }
 }
