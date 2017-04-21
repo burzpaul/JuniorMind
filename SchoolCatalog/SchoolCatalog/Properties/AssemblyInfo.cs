fa380e753943eaa0b016a8863885953ddfce09e8
Paul Burz <burzpaul@yahoo.ro>
2017-04-21 11:16:41 +0300

    Add SchoolCatalog empty project and Classes class, student class and tests for them

---
+++ SchoolCatalog/SchoolCatalog/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SchoolCatalog")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SchoolCatalog")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4431531f-1f43-4b52-96ed-b7d19028e5e4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SchoolCatalog/SchoolCatalog/StudentTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SchoolCatalog
+{
+    [TestClass]
+    public class StudentTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+        [TestMethod]
+        public void Test3()
+        {
+
+        }
+        [TestMethod]
+        public void Test3()
+        {
+
+        }
+    }
+}
