e436472eea03f497d15a2d8829e338d18f149bec
Paul Burz <burzpaul@yahoo.ro>
2017-04-21 13:59:21 +0300

    Add StudentsOrderedByGpa class and test for it

---
+++ SchoolCatalog/SchoolCatalog/StudentsOrderedByGPATests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SchoolCatalog
+{
+    [TestClass]
+    public class OrderByGPATests
+    {
+        [TestMethod]
+        public void Test_If_Ordered_By_GPA()
+        {    
+        }
+    }
+}
