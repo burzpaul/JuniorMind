38fbb558201ed0f2545de2746d6f18482f488b47
Paul Burz <burzpaul@yahoo.ro>
2017-04-21 11:24:58 +0300

    Add Grades class and test for it

---
+++ SchoolCatalog/SchoolCatalog/GradesTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SchoolCatalog
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
