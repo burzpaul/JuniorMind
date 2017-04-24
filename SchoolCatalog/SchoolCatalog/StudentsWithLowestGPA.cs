32a1f062c90a6d8c3f20e44560f948f3fec72cf1
Paul Burz <burzpaul@yahoo.ro>
2017-04-24 10:11:19 +0300

    Students With Lowest GPA

---
+++ SchoolCatalog/SchoolCatalog/StudentsWithLowestGPA.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolCatalog
+{
+    class StudentsWithLowestGPA
+    {
+        private Student[] students;
+        private int index;
+
+        public StudentsWithLowestGPA(Student[] students)
+        {
+            this.students = students;
+            index = students.Length - 1;
+            StudentsOrderedByGPA();
+        }
+
+        public bool GetNext(out Student student)
+        {
+            
+            if (index > -1)
+            {
+                student = this.students[index--];
+                return true;
+            }
+            student = null;
+            return false;
+        }
+
+        private void StudentsOrderedByGPA()
+        {
+            new StudentsOrderedByGPA(students);    
+        }
+    }
+}
---
+++ SchoolCatalog/SchoolCatalog/StudentsWithLowestGPATests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SchoolCatalog
+{
+    [TestClass]
+    public class StudentsWithLowestGPATests
+    {
+        [TestMethod]
+        public void Students_With_Lowest_Gpa ()
+        {
+            var students = new Student[] { new Student("Carl", new Subject[] { new Subject("Economics", new Grades(new int[] { 10, 7, 10, 10 })) }),
+                                            new Student("Medusa", new Subject[] { new Subject("Economics", new Grades(new int[] { 6, 7, 8, 2 })) }),
+                                            new Student("Trump", new Subject[] { new Subject("Economics", new Grades(new int[] { 10, 10, 10, 10 })) }),
+                                            new Student("Melania", new Subject[] { new Subject("Economics", new Grades(new int[] { 6, 7, 8, 5 })) }),
+                                            new Student("Tuck", new Subject[] { new Subject("Economics", new Grades(new int[] { 10, 10, 8, 7 })) }),
+                                            new Student("Tom", new Subject[] { new Subject("Economics", new Grades(new int[] { 6, 7, 8, 10 })) })};
+
+            var expected = students[1];
+
+            var expcted2 = students[3];
+
+            var studentsWithLowestGPA = new StudentsWithLowestGPA(students);
+
+            Student student;
+
+            Assert.IsTrue(studentsWithLowestGPA.GetNext(out student));
+            Assert.AreEqual(true, expected.IsSameStudent(student));
+            Assert.IsTrue(studentsWithLowestGPA.GetNext(out student));
+            Assert.AreEqual(true, expcted2.IsSameStudent(student));
+        }
+    }
+}
