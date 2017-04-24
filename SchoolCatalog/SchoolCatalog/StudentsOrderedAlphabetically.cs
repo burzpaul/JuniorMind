137dcbca39de8e7e2806229a381fe02ce97b9c6f
Paul Burz <burzpaul@yahoo.ro>
2017-04-24 16:19:13 +0300

    Add Interface1

---
+++ SchoolCatalog/SchoolCatalog/StudentsOrderedAlphabetically.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolCatalog
+{
+    class StudentsOrderedAlphabetically : IEnumerable<Student>
+    {
+        private Student[] students;
+
+        public StudentsOrderedAlphabetically(Student[] candidate)
+        {
+            this.students = candidate;
+            HeapSort();
+        }
+
+        private void HeapSort()
+        {
+            int n = students.Length;
+            for (int i = n / 2 - 1; i >= 0; i--)
+            {
+                Heapify(n, i);
+            }
+            for (int i = n - 1; i >= 0; i--)
+            {
+                Swap(ref students[0], ref students[i]);
+
+                Heapify(i, 0);
+            }
+        }
+
+        private void Heapify(int n, int root)
+        {
+            int largest = root;
+            int left = 2 * root + 1;
+            int right = 2 * root + 2;
+
+            if (left < n && students[left].WhoIsFirstAlphabetically(students[largest]) != -1)
+            {
+                largest = left;
+            }
+            if (right < n && students[right].WhoIsFirstAlphabetically(students[largest]) != -1)
+            {
+                largest = right;
+            }
+            if (largest != root)
+            {
+                Swap(ref students[root], ref students[largest]);
+                Heapify(n, largest);
+            }
+        }
+
+        private static void Swap(ref Student student1, ref Student student2)
+        {
+            var temp = student1;
+            student1 = student2;
+            student2 = temp;
+        }
+
+        public IEnumerator<Student> GetEnumerator()
+        {
+            foreach(var student in students)
+            {
+                yield return student;
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ SchoolCatalog/SchoolCatalog/StudentsOrderedAlphabeticallyTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SchoolCatalog
+{
+    [TestClass]
+    public class StudentsOrderedAlphabeticallyTests
+    {
+        [TestMethod]
+        public void Test_If_Ordered_Alphabetically()
+        {
+            var students = new Student[] { new Student("Paul", new Subject[] { }),
+                                          new Student("Clark",new Subject[]{ }),
+                                          new Student("Alex", new Subject[] { }),
+                                          new Student("Seinfeld",new Subject[]{ })};
+
+            var expected = new Student[] { students[2] };
+
+            var sortedAlphabetically = new StudentsOrderedAlphabetically(students);
+
+            var actual = sortedAlphabetically.GetEnumerator();
+
+            actual.MoveNext();           
+
+            Assert.AreEqual(false, expected[0].IsSameStudent(actual.Current));
+        }
+    }
+}
