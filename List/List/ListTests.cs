febee14163365f6415e3fca584d7558fa06b4f80
Paul Burz <burzpaul@yahoo.ro>
2017-04-28 12:31:01 +0300

    Add new List project

---
+++ List/List/ListTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace List
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Add_Test()
+        {
+            var a = new List<string> { "a", "b", "c" };
+            CollectionAssert.AreEqual(new[] { "a", "b", "c" }, a.ToArray());
+        }
+
+        [TestMethod]
+        public void Insert_Test()
+        {
+            var list = new List<string>();
+            list.Add("a");
+            list.Add("b");
+            list.Add("d");
+            list.Insert(2, "c");
+
+            var b = list.GetEnumerator();
+            b.MoveNext();
+            b.MoveNext();
+            b.MoveNext();
+            Assert.AreEqual("c", b.Current);
+        }
+        [TestMethod]
+        public void Clear_Test()
+        {
+            var list = new List<string>();
+            list.Add("a");
+            list.Add("b");
+            list.Add("c");
+
+            list.Clear();
+            Assert.AreEqual(0, list.Count);
+        }
+        [TestMethod]
+        public void Contains_Test()
+        {
+            var list = new List<string>();
+            list.Add("a");
+            list.Add("b");
+            list.Add("c");
+
+            Assert.IsTrue(list.Contains("b"));
+        }
+        [TestMethod]
+        public void Index_Of_Test()
+        {
+            var list = new List<string>();
+            list.Add("a");
+            list.Add("b");
+            list.Add("c");
+
+            Assert.AreEqual(1, list.IndexOf("b"));
+        }
+        [TestMethod]
+        public void Remove_Test()
+        {
+            var list = new List<string>();
+            list.Add("a");
+            list.Add("b");
+            list.Add("c");
+
+            list.Remove("a");
+
+            Assert.IsFalse(list.Contains("a"));
+
+            var b = list.GetEnumerator();
+            b.MoveNext();
+
+            Assert.AreEqual("b", b.Current);
+        }
+    }
+}
---
+++ List/List/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("List")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("List")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("01cb8dbd-465e-4ae5-8dce-c0837974021f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
