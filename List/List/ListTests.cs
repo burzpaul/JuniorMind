2a0f07db6f0850f191418ec4efbc7d5e4692cfe4
Paul Burz <burzpaul@yahoo.ro>
2017-04-28 12:41:23 +0300

    Added more tests to ListTests

--- List/List/ListTests.cs
+++ List/List/ListTests.cs
@@ -5,22 +5,58 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace List
 {
     [TestClass]
-    public class UnitTest1
+    public class ListTests
     {
         [TestMethod]
         public void Add_Test()
         {
-            var a = new List<string> { "a", "b", "c" };
-            CollectionAssert.AreEqual(new[] { "a", "b", "c" }, a.ToArray());
+            var list = new List<string> { "a", "b", "c" };
+            list.Add("d");
+            CollectionAssert.AreEqual(new[] { "a", "b", "c","d" }, list.ToArray());
         }
+        [TestMethod]
+        public void Count_Test()
+        {
+            var list = new List<int> { 1, 2, 3, 4 };
 
+            Assert.AreEqual(4, list.Count);
+        }
+        [TestMethod]
+        public void Clear_Test()
+        {
+            var list = new List<string> { "a", "b", "c" };
+
+            list.Clear();
+            Assert.AreEqual(0, list.Count);
+        }
+        [TestMethod]
+        public void Contains_Test()
+        {
+            var list = new List<string> { "a", "b", "c" };
+
+            Assert.IsTrue(list.Contains("b"));
+        }
+        [TestMethod]
+        public void CopyTo_Test()
+        {
+            var list = new List<char> { 'p', 'a', 'u', 'l' };
+            var listCopy = new char[list.Count];
+
+            list.CopyTo(listCopy, 0);
+
+            CollectionAssert.AreEqual(listCopy ,list.ToArray());
+        }
+        [TestMethod]
+        public void Index_Of_Test()
+        {
+            var list = new List<string>() { "a", "b", "c" };
+
+            Assert.AreEqual(1, list.IndexOf("b"));
+        }
         [TestMethod]
         public void Insert_Test()
         {
-            var list = new List<string>();
-            list.Add("a");
-            list.Add("b");
-            list.Add("d");
+            var list = new List<string> { "a", "b", "c" };
             list.Insert(2, "c");
 
             var b = list.GetEnumerator();
@@ -30,43 +66,9 @@ namespace List
             Assert.AreEqual("c", b.Current);
         }
         [TestMethod]
-        public void Clear_Test()
-        {
-            var list = new List<string>();
-            list.Add("a");
-            list.Add("b");
-            list.Add("c");
-
-            list.Clear();
-            Assert.AreEqual(0, list.Count);
-        }
-        [TestMethod]
-        public void Contains_Test()
-        {
-            var list = new List<string>();
-            list.Add("a");
-            list.Add("b");
-            list.Add("c");
-
-            Assert.IsTrue(list.Contains("b"));
-        }
-        [TestMethod]
-        public void Index_Of_Test()
-        {
-            var list = new List<string>();
-            list.Add("a");
-            list.Add("b");
-            list.Add("c");
-
-            Assert.AreEqual(1, list.IndexOf("b"));
-        }
-        [TestMethod]
         public void Remove_Test()
         {
-            var list = new List<string>();
-            list.Add("a");
-            list.Add("b");
-            list.Add("c");
+            var list = new List<string> { "a", "b", "c" };
 
             list.Remove("a");
 
@@ -76,6 +78,15 @@ namespace List
             b.MoveNext();
 
             Assert.AreEqual("b", b.Current);
+        }
+        [TestMethod]
+        public void RemoveAt_Test()
+        {
+            var list = new List<double> { 2.21, 5.32, 7.89, 9.72 };
+
+            list.RemoveAt(2);
+
+            CollectionAssert.AreEqual(new[] { 2.21, 5.32, 9.72 }, list.ToArray());
         }
     }
 }
