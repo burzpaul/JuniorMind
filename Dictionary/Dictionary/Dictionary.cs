aef75a571352a1b091898d77902e37498328df39
Paul Burz <burzpaul@yahoo.ro>
2017-05-05 09:31:17 +0300

    Add Dictionary Project

--- Dictionary/Dictionary/Dictionary.cs
+++ Dictionary/Dictionary/Dictionary.cs
@@ -9,73 +9,195 @@ namespace Dictionary
 {
     public class Dictionary<TKey, TValue> : IDictionary<TKey, TValue>
     {
+        class Entry
+        {
+            public TKey key;
+            public TValue value;
+            public int next = -1;
+
+            public Entry(TKey key, TValue value, int next)
+            {
+                this.key = key;
+                this.value = value;
+                this.next = next;
+            }
+        }
+
+        private int[] buckets;
+        private Entry[] entry;
+        private int elementsCount = 0;
+        private int count = 0;
+        private int nextFreeSpace = -1;
+
+        public Dictionary(int size)
+        {
+            buckets = new int[size];
+            entry = new Entry[size];
+            for (int i = 0; i < entry.Length; i++)
+            {
+                buckets[i] = -1;
+            }
+        }
+
+        public ICollection<TKey> Keys => throw new NotImplementedException();//Not Implemented
+
+        public ICollection<TValue> Values => throw new NotImplementedException();//Not Implemented
+
+        public int Count
+        {
+            get
+            {
+                return elementsCount;
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                return false;
+            }
+        }
+
         public TValue this[TKey key]
         {
-            get => throw new NotImplementedException();
-            set => throw new NotImplementedException();
-        }
-
-        public ICollection<TKey> Keys => throw new NotImplementedException();
-
-        public ICollection<TValue> Values => throw new NotImplementedException();
-
-        public int Count => throw new NotImplementedException();
-
-        public bool IsReadOnly => throw new NotImplementedException();
-
-        public void Add(TKey key, TValue value)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Add(KeyValuePair<TKey, TValue> item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Clear()
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool Contains(KeyValuePair<TKey, TValue> item)
-        {
-            throw new NotImplementedException();
+            get
+            {
+                return entry[FindIndex(key)].value;
+            }
+            set
+            {
+                entry[FindIndex(key)].value = value;
+            }
         }
 
         public bool ContainsKey(TKey key)
         {
-            throw new NotImplementedException();
+            return FindIndex(key) > -1;
         }
 
-        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+        public bool Contains(KeyValuePair<TKey, TValue> item)
         {
-            throw new NotImplementedException();
+            return FindIndex(item.Key) > -1;
         }
 
-        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        private int FindIndex(TKey key)
         {
-            throw new NotImplementedException();
+            int bucketIndex = GetKeyIndex(key);
+            for (var index = buckets[bucketIndex]; index > -1; index = entry[index].next)
+            {
+                if (entry[index].key.Equals(key))
+                {
+                    return index;
+                }
+            }
+            return -1;
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            var bucketIndex = GetKeyIndex(key);
+            var index = GetNextFreeSpace();
+            entry[index] = new Entry(key, value, buckets[bucketIndex]);
+            buckets[bucketIndex] = index;
+            count++;
+            elementsCount++;
+        }
+
+        private int GetKeyIndex(TKey key)
+        {
+            return Math.Abs(key.GetHashCode() % buckets.Length);
         }
 
         public bool Remove(TKey key)
         {
-            throw new NotImplementedException();
+            int bucketIndex = GetKeyIndex(key);
+            int previous = -1;
+            for (var index = buckets[bucketIndex]; index > -1; index = entry[index].next)
+            {
+                if (entry[index].key.Equals(key))
+                {
+                    RemoveEntry(bucketIndex, previous, index);
+                    elementsCount--;
+                    return true;
+                }
+                previous = index;
+            }
+            return false;
         }
 
-        public bool Remove(KeyValuePair<TKey, TValue> item)
+        private int GetNextFreeSpace()
         {
-            throw new NotImplementedException();
+            if (nextFreeSpace > -1)
+            {
+                var index = nextFreeSpace;
+                nextFreeSpace = entry[nextFreeSpace].next;
+                return index;
+            }
+            return count;   
+        }
+
+        private void RemoveEntry(int bucketIndex, int previous, int index)
+        {
+            if (previous < 0)
+            {
+                buckets[bucketIndex] = entry[index].next;
+
+            }
+            else
+            {
+                entry[previous].next = entry[index].next;
+
+            }
+            entry[index].key = default(TKey);
+            entry[index].value = default(TValue);
+            entry[index].next = nextFreeSpace;
+            nextFreeSpace = index;
         }
 
         public bool TryGetValue(TKey key, out TValue value)
         {
             throw new NotImplementedException();
+        }//Not Implemented
+
+        public void Add(KeyValuePair<TKey, TValue> item)
+        {
+            throw new NotImplementedException();
+        }//Not Implemented
+
+        public void Clear()
+        {
+            for (int i = 0; i < buckets.Length; i++)
+            {
+                entry[i] = null;
+                buckets[i] = -1;
+            }
+            elementsCount = 0;
+            elementsCount = 0;
+        }
+
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }//Not Implemented
+
+        public bool Remove(KeyValuePair<TKey, TValue> item)
+        {
+            throw new NotImplementedException();
+        }//Not Implemented
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            for (int i = 0; i < entry.Length; i++)
+            {
+                yield return new KeyValuePair<TKey, TValue>(entry[i].key, entry[i].value);
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return GetEnumerator();
         }
     }
+
 }
--- Dictionary/Dictionary/DictionaryTests.cs
+++ Dictionary/Dictionary/DictionaryTests.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Dictionary
 {
@@ -7,8 +9,129 @@ namespace Dictionary
     public class DictionaryTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Count_Test()
         {
+            var dictionary = new Dictionary<string, int>(3) { { "zero", 0 }, { "one", 1 }, { "two", 2 } };
+
+            Assert.AreEqual(3, dictionary.Count);
+        }
+        [TestMethod]
+        public void Contains_Key_True()
+        {
+            var dictionary = new Dictionary<string, int>(2) { { "one", 1 }, { "two", 2 } };
+
+            Assert.IsTrue(dictionary.ContainsKey("one"));
+        }
+        [TestMethod]
+        public void Contains_Key_False()
+        {
+            var dictionary = new Dictionary<string, int>(2) { { "one", 1 }, { "two", 2 } };
+
+            Assert.IsFalse(dictionary.ContainsKey("three"));
+        }
+        [TestMethod]
+        public void Contains_Key_Value_Pair_True()
+        {
+            var dictionary = new Dictionary<char, int>(2) { { 'a', 1 }, { 'b', 4 } };
+
+            Assert.IsTrue(dictionary.Contains(new KeyValuePair<char, int>('a', 1)));
+        }
+        [TestMethod]
+        public void Contains_Key_Value_Pair_False()
+        {
+            var dictionary = new Dictionary<char, int>(2) { { 'a', 1 }, { 'b', 2 } };
+
+            Assert.IsFalse(dictionary.Contains(new KeyValuePair<char, int>('c', 3)));
+        }
+        [TestMethod]
+        public void TValue_Test_Get()
+        {
+            var dictionary = new Dictionary<double, int>(4) { { 1.21, 0 }, { 32.1, 1 }, { 22.5, 3 }, { 656.4, 4 } };
+
+            Assert.AreEqual(3,dictionary[22.5]);
+        }
+        [TestMethod]
+        public void TValue_Test_Set()
+        {
+            var dictionary = new Dictionary<double, int>(4) { { 1.21, 0 }, { 32.1, 1 }, { 22.5, 3 }, { 656.4, 4 } };
+
+            dictionary[22.5] = 321;
+
+            Assert.AreEqual(321, dictionary[22.5]);
+        }
+        [TestMethod]
+        public void Remove_Test()
+        {
+            var dictionary = new Dictionary<char, int>(4) { { 'a', 1 }, { 'b', 2 }, { 'c', 3 }, { 'd', 4 } };
+
+            dictionary.Remove('b');
+
+            Assert.IsFalse(dictionary.Contains(new KeyValuePair<char, int>('b', 2)));
+        }
+        [TestMethod]
+        public void Remove_With_Conflicts()
+        {
+            var dictionary = new Dictionary<int, string>(10) { { 1, "zero" }, { 11, "one" }, { 2, "two" }, { 21, "three" } };
+
+            dictionary.Remove(11);
+
+            Assert.IsTrue(dictionary.Contains(new KeyValuePair<int, string>(1, "zero")));
+            Assert.IsFalse(dictionary.Contains(new KeyValuePair<int, string>(11, "one")));
+        }
+        [TestMethod]
+        public void Clear_Test()
+        {
+            var dictionary = new Dictionary<int, string>(5) { { 1, "a" }, { 2, "b" }, { 3, "c" }, { 4, "d" }, { 5, "e" } };
+
+            dictionary.Clear();
+
+            Assert.IsFalse(dictionary.Contains(new KeyValuePair<int, string>(1, "a")));
+            Assert.IsFalse(dictionary.Contains(new KeyValuePair<int, string>(2, "b")));
+            Assert.IsFalse(dictionary.Contains(new KeyValuePair<int, string>(3, "c")));
+            Assert.IsFalse(dictionary.Contains(new KeyValuePair<int, string>(4, "d")));
+            Assert.IsFalse(dictionary.Contains(new KeyValuePair<int, string>(5, "e")));
+        }
+        [TestMethod]
+        public void Enumerator_Test()
+        {
+            var dictionary = new Dictionary<int, string>(10) { { 1, "zero" }, { 11, "one" }, { 2, "two" }, { 21, "three" } };
+
+            var actual = dictionary.GetEnumerator();
+
+            actual.MoveNext();
+
+            Assert.AreEqual(actual.Current,new KeyValuePair<int,string>(1,"zero"));
+
+            actual.MoveNext();
+
+            Assert.AreEqual(actual.Current, new KeyValuePair<int, string>(11, "one"));
+        }
+        [TestMethod]
+        public void Next_Free_Space_Test()
+        {
+            var dictionary = new Dictionary<string, int>(1) { { "one", 1 } };
+
+            dictionary.Remove("one");
+
+            dictionary.Add("four", 4);
+
+            Assert.IsTrue(dictionary.Contains(new KeyValuePair<string, int>("four", 4)));
+        }
+
+        [TestMethod]
+        public void Next_Free_Space_With_Multiple_Test()
+        {
+            var dictionary = new Dictionary<string, int>(5) { { "zero", 123}, { "one", 1 }, { "two", 12 }, { "five", 325 }, { "six", 6232 } };
+
+            dictionary.Remove("one");
+            dictionary.Remove("five");
+
+            dictionary.Add("four", 4);
+            dictionary.Add("three", 3);
+            
+
+            Assert.IsTrue(dictionary.ContainsKey("three"));
+            Assert.IsTrue(dictionary.ContainsKey("four"));
         }
     }
 }
