d824c161a1d2373a68ed4a1059e2d2d1e6e6d8ad
Paul Burz <burzpaul@yahoo.ro>
2017-05-08 17:24:29 +0300

    Implement The Two GET methods

--- ToDoApi/ToDoApi/Controllers/TodoController.cs
+++ ToDoApi/ToDoApi/Controllers/TodoController.cs
@@ -1,46 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
-namespace ToDoApi.Controllers
+namespace TodoApi.Controllers
 {
     [Route("api/[controller]")]
     public class TodoController : Controller
     {
-        // GET: api/values
+        private readonly ITodoRepository _todoRepository;
+
+        public TodoController(ITodoRepository todoRepository)
+        {
+            _todoRepository = todoRepository;
+        }
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<TodoItem> GetAll()
         {
-            return new string[] { "value1", "value2" };
+            return _todoRepository.GetAll();
         }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        [HttpGet("{id}", Name = "GetTodo")]
+        public IActionResult GetById(long id)
         {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            var item = _todoRepository.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
         }
     }
 }
