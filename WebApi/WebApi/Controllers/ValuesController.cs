63bc2b991cb8dd2e0f610d5baef75cbfd652926c
Paul Burz <burzpaul@yahoo.ro>
2017-05-12 17:15:09 +0300

    WebApi Project

---
+++ WebApi/WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Html;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private PasswordInfo info = new PasswordInfo();
+    
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Json(new PasswordGenerator(info).GeneratePassword());
+        }
+
+        [HttpGet("{id}",Name = "Passwords")]
+        public IActionResult Get(int numberOf)
+        {
+            return Json(new PasswordGenerator(info).GeneratePassword());
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody]PasswordInfo info)
+        {
+            if (info.Number <= 0) 
+            {
+                return BadRequest(new ExceptionClas { NumberOfPasswords = info.Number,
+                                    Message = "number of passwords has to ge greater than 0" });
+            }
+            if (info.PasswordLength < 8)
+            {
+                return BadRequest("Password must contain more than 8 characters");
+            }
+            return new ObjectResult(new PasswordGenerator(info).GeneratePassword());
+        }
+
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+
+    public class ExceptionClas
+    {
+        public int NumberOfPasswords { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ WebApi/WebApi/PasswordInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi
+{
+    public class PasswordInfo
+    {
+        public int Number { get; set; }
+        public int PasswordLength { get; set; }
+        public int UpperCase { get; set; }
+        public int Digits { get; set; }
+        public int Symbols { get; set; }
+        public bool ExcludeSimilar { get; set; }
+        public bool ExcludeAmbigous { get; set; }
+    }
+}
