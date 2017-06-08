2cf889fa363c15c5f92559970f743ce49dc2e67c
Paul Burz <burzpaul@yahoo.ro>
2017-06-08 11:28:43 +0300

    Add WebApiReact project

---
+++ WebApiReact/WebApiReact/Controllers/ValuesController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Cors;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private PasswordInfo info = new PasswordInfo()
+        {
+            NumberOfPasswords = 1,
+            PasswordLength = 8,
+            UpperCase = 2,
+            Digits = 2,
+            Symbols = 2,
+            ExcludeSimilar = false,
+            ExcludeAmbigous = false
+        };
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Json(new PasswordGenerator(info).GeneratePassword());
+        }
+
+        [HttpGet("{id}", Name = "Passwords")]
+        public IActionResult Get(int numberOf)
+        {
+            return Json(new PasswordGenerator(info).GeneratePassword());
+        }
+
+        [HttpGet]
+        [Route("defaultvalues")]
+        public IActionResult GetInfo()
+        {
+            var result = new
+            {
+                NumberOfPasswords = info.NumberOfPasswords,
+                PasswordLength = info.PasswordLength,
+                UpperCase = info.UpperCase,
+                Digits = info.Digits,
+                Symbols = info.Symbols,
+                ExcludeSimilar = info.ExcludeSimilar,
+                ExcludeAmbigous = info.ExcludeAmbigous
+            };
+
+            return new ObjectResult(result);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody]PasswordInfo info)
+        {
+            if (info.NumberOfPasswords <= 0)
+            {
+                return BadRequest(new ExceptionClas
+                {
+                    NumberOfPasswords = info.NumberOfPasswords,
+                    Message = "number of passwords has to ge greater than 0"
+                });
+            }
+            if (info.PasswordLength < 8)
+            {
+                return BadRequest("Password must contain more than 8 characters");
+            }
+            return new ObjectResult(new PasswordGenerator(info).GeneratePassword());
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+
+    public class ExceptionClas
+    {
+        public int NumberOfPasswords { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ WebApiReact/WebApiReact/PasswordGenerator.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi
+{
+    public class PasswordGenerator
+    {
+        private PasswordInfo info;
+        private Random rand = new Random();
+
+        public PasswordGenerator(PasswordInfo info)
+        {
+            this.info = info;
+        }
+
+        public string[] GeneratePassword()
+        {
+            string[] passwords = new string[info.NumberOfPasswords];
+            passwords = Enumerable.Range(0, info.NumberOfPasswords).Select(x => GenerateOnePassword()).ToArray();
+            return passwords;
+        }
+
+        private string GenerateOnePassword()
+        {
+            var password = GetCharacters((info.PasswordLength - info.UpperCase - info.Symbols - info.Digits), 'a', 'z', info.ExcludeSimilar) +
+                            GetCharacters(info.UpperCase, 'A', 'Z', info.ExcludeSimilar) +
+                            GetSymbols(info.Symbols, info.ExcludeAmbigous) +
+                            GetCharacters(info.Digits, '0', '9', info.ExcludeSimilar);
+
+            var shuffled = Shuffle(password);
+            return password;
+        }
+
+        private string Shuffle(string str)
+        {
+            char[] array = str.ToCharArray();
+            for (int i = 0; i <= rand.Next(); i++)
+            {
+                rand.Next();
+            }
+            int n = array.Length;
+            while (n > 1)
+            {
+                n--;
+                int k = rand.Next(n + 1);
+                var value = array[k];
+                array[k] = array[n];
+                array[n] = value;
+            }
+            return new string(array);
+        }
+
+        private string GetCharacters(int numberOfCharacters, char bottomBoundry, char upperBoundry, bool var)
+        {
+            string result = null;
+            char temporaryHolder;
+            string specialCharacters = "l1Io0O";
+            for (int i = 0; i < numberOfCharacters; i++)
+            {
+                temporaryHolder = (char)rand.Next(bottomBoundry, upperBoundry);
+                if (var == true && specialCharacters.IndexOf(temporaryHolder) != -1)
+                {
+                    i--;
+                    continue;
+                }
+                result += temporaryHolder;
+            }
+            return result;
+        }
+
+        private string GetSymbols(int numberOfSymbols, bool var)
+        {
+            string symbols = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+            string specialSymbols = "{}[]()/\'~,;.<>";
+            char temporaryHolder;
+            string result = null;
+            for (int i = 0; i < numberOfSymbols; i++)
+            {
+                temporaryHolder = symbols[rand.Next(0, 30)];
+                if (var == true && specialSymbols.IndexOf(temporaryHolder) != -1)
+                {
+                    i--;
+                    continue;
+                }
+                result += temporaryHolder;
+            }
+            return result;
+        }
+    }
+}
