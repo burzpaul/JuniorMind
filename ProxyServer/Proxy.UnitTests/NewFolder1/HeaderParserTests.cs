759826ed28d99bdbd03b1d32487e2e7d49537c09
Paul Burz <burzpaul@yahoo.ro>
2017-08-08 13:57:36 +0300

    Proxy work in progress

---
+++ ProxyServer/Proxy.UnitTests/NewFolder1/HeaderParserTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proxy.UnitTests.Tests
+{
+    class HeaderParserTests
+    {
+    }
+}
---
+++ ProxyServer/Proxy.UnitTests/ProxyServer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy.UnitTests
+{
+    internal class ProxyServer : IDisposable
+    {
+        public int BufferSize { get; set; } = 4096;
+
+        public List<ProxyEndPoint> ProxyEndPoints { get; set; }
+
+        public void Start()
+        {
+            ProxyEndPoints = new List<ProxyEndPoint>(10);
+            foreach (var endPoint in ProxyEndPoints)
+            {
+                Listen(endPoint);
+            }
+        }
+
+        private async void Listen(ProxyEndPoint endPoint)
+        {
+            endPoint.Listener = new TcpListener(endPoint.IpAddress, endPoint.Port);
+            endPoint.Listener.Start();
+            Console.WriteLine("Proxy started. Awaiting connections.\n");
+
+            endPoint.Port = ((IPEndPoint)endPoint.Listener.LocalEndpoint).Port;
+
+            while (true)
+            {
+                TcpClient client = await endPoint.Listener.AcceptTcpClientAsync();
+                if (client != null)
+                {
+                    Console.WriteLine($"Connection established on port: {client.Client.LocalEndPoint} \n");
+                    Task.Run(async () => this);
+                }
+            }
+        }
+
+        public void Stop(ProxyEndPoint endPoint)
+        {
+            endPoint.Listener.Stop();
+            endPoint.Listener.Server.Dispose();
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
